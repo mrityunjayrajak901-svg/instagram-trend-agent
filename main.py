2aa4b9e2ef0586015537c127aeed908607104ff0
mrityunjayrajak901-svg <mrityunjayrajak901@gmail.com>
2026-05-01 02:24:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-from telegram import Bot
-import requests
-
 TOKEN = "8669023960:AAEw3DZdH2RhCK3WvRg3_fdYImafG0QKrrk"
 CHAT_ID = "7008909688"
 OPENROUTER_API_KEY = "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"
 
-bot = Bot(token=TOKEN)
-
 prompt = """
-Give today's viral Instagram AI photo editing trend.
+Give today's viral Instagram AI editing trend.
+
 Include:
 
-1. Trend name
-2. Viral prediction
-3. Cinematic editing prompt
+1. Trend Name
+2. Viral Score
+3. Cinematic Prompt
 4. Caption
 5. Hashtags
    """
 
 response = requests.post(
-url="https://openrouter.ai/api/v1/chat/completions",
+"https://openrouter.ai/api/v1/chat/completions",
 headers={
 "Authorization": f"Bearer {OPENROUTER_API_KEY}",
 "Content-Type": "application/json"
@@ -27,7 +23,10 @@ headers={
 json={
 "model": "deepseek/deepseek-chat",
 "messages": [
-{"role": "user", "content": prompt}
+{
+"role": "user",
+"content": prompt
+}
 ]
 }
 )
@@ -36,4 +35,14 @@ data = response.json()
 
 message = data["choices"][0]["message"]["content"]
 
-bot.send_message(chat_id=CHAT_ID, text=message)
+telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(
+telegram_url,
+data={
+"chat_id": CHAT_ID,
+"text": message
+}
+)
+
+print("Message sent successfully")
