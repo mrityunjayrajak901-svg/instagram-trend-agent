b78d89f98eeb6e67e95f6e49835c7e16c0963844
mrityunjayrajak901-svg <mrityunjayrajak901@gmail.com>
2026-05-01 03:23:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import requests
 
-=========================
 
 YOUR CREDENTIALS
 
-=========================
 
 TOKEN = "8669023960:AAEw3DZdH2RhCK3WvRg3_fdYImafG0QKrrk"
 CHAT_ID = "7008909688"
 OPENROUTER_API_KEY = "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"
 
-=========================
 
 AI PROMPT
 
-=========================
 
 prompt = """
 You are an Instagram AI Trend Prediction Agent.
@@ -36,11 +32,9 @@ Focus on:
 - Emotional storytelling visuals
   """
 
-=========================
 
 OPENROUTER API REQUEST
 
-=========================
 
 response = requests.post(
 "https://openrouter.ai/api/v1/chat/completions",
@@ -59,11 +53,9 @@ json={
 }
 )
 
-=========================
 
 RESPONSE HANDLE
 
-=========================
 
 data = response.json()
 
@@ -74,11 +66,9 @@ message = data["choices"][0]["message"]["content"]
 else:
 message = f"API Error:\n{data}"
 
-=========================
 
 SEND TO TELEGRAM
 
-=========================
 
 telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
