3634aaa873f2d52144ccf5d50ee4ba29ebf58cb8
mrityunjayrajak901-svg <mrityunjayrajak901@gmail.com>
2026-05-01 06:16:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ CHAT_ID = "7008909688"
 OPENROUTER_API_KEY = "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"
 
 prompt = """
-You are an Instagram AI Trend Prediction Agent.
+Give today's viral Instagram AI editing trend with:
 
-Give:
-
-1. Today's viral Instagram AI editing trend
-2. Viral Score
-3. Cinematic AI photo editing prompt
+1. Trend name
+2. Viral score
+3. Editing prompt
 4. Caption
 5. Hashtags
    """
@@ -24,30 +22,17 @@ headers={
 },
 json={
 "model": "openai/gpt-3.5-turbo",
-"messages": [
-{
-"role": "user",
-"content": prompt
-}
-]
+"messages": [{"role": "user", "content": prompt}]
 }
 )
 
 data = response.json()
 
-if "choices" in data:
-   message= data["choices"][0]["message"]["content"]
-else:  
-   message= str(data)
-
-telegram_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+message = data.get("choices", [{}])[0].get("message", {}).get("content", str(data))
 
 requests.post(
-telegram_url,
-data={
-"chat_id": CHAT_ID,
-"text": message
-}
+f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+data={"chat_id": CHAT_ID, "text": message}
 )
 
-print("Message sent successfully")
+print("Done")
