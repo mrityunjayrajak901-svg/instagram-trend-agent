a8a94fb05bc186900d948579beab051d111798bf
mrityunjayrajak901-svg <mrityunjayrajak901@gmail.com>
2026-05-01 18:41:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,35 @@ import requests
 
 TOKEN = "8669023960:AAEw3DZdH2RhCK3WvRg3_fdYImafG0QKrrk"
 CHAT_ID = "7008909688"
-OPENROUTER_API_KEY = "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"
 
-prompt = "Give one viral Instagram AI photo editing trend with cinematic prompt, caption and hashtags."
+prompt = """
+Give one viral Instagram AI photo editing trend.
+Include:
+
+1. Trend Name
+2. Cinematic Prompt
+3. Caption
+4. Hashtags
+   """
+
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
 
 response = requests.post(
-url="https://openrouter.ai/api/v1/chat/completions",
-headers={
-"Authorization": f"Bearer {OPENROUTER_API_KEY}",
-"Content-Type": "application/json",
-"HTTP-Referer": "https://openrouter.ai",
-"X-Title": "Instagram Trend Agent"
-},
+API_URL,
 json={
-"model": "mistralai/mistral-7b-instruct:free",
-"messages": [
-{
-"role": "user",
-"content": prompt
-}
-]
+"inputs": prompt
 }
 )
 
 data = response.json()
 
-if "choices" in data:
-   message = data["choices"][0]["message"]["content"]
-else:
-   message = str(data)
+message = str(data)
 
 requests.post(
 f"https://api.telegram.org/bot{TOKEN}/sendMessage",
 data={
 "chat_id": CHAT_ID,
-"text": message
+"text": message[:3500]
 }
 )
 
